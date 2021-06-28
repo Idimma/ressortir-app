f7bc72f15a8cf716093f00d856e25134d1bc82a1
Idowu Emmanuel <idowu.immanuel17@gmail.com>
2021-06-28 15:20:08 +0100

    news

--- src/services/index.js
+++ src/services/index.js
@@ -4,14 +4,11 @@ import CategoryService from './category.service';
 import ProductService from './product.service';
 import CartService from './carts.service';
 import BookingService from './booking.service';
-// import WalletService from './wallet.service';
-// import SettingsService from './settings.service';
 import CardService from './card.service';
 import PayCodeService from './paycode.service';
 import OrderService from './order.service';
 import PromoService from './promo.service';
 import PaystackService from './paystack.service';
-// import UserService from './user.service';
 
 export {
   AuthService, CartService,
@@ -22,9 +19,6 @@ export {
   ProductService,
   PromoService,
   CategoryService,
-  // WalletService,
-  // SettingsService,
   OrderService,
-  // UserService
 };
 
